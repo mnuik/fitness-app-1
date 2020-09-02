0ee1ec16a43f09345316cc871d1035d1ca375aea
mnuik <63105966+mnuik@users.noreply.github.com>
2020-09-02 21:00:22 +0300

    yay frontend

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, } from 'react'
+import { Button } from 'reactstrap'
+import moment from 'moment'
+
+import api from '../../services/api'
+import './dashboard.css'
+
+export default Dashboard => {
+    const user_id = localStorage.getItem('user')
+
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    const getEvents = async(params) => {
+        const url = params ? `/dashboard/${params}` : '/dashboard'
+        const response = await api.get(url, {headers: {user_id}})
+
+        setEvents(response.data)
+    }
+
+    return (
+        <ul className="events-list">
+            {events.map(event =>
+                <li key={event._id}>
+                <header style= {{ backgroundImage: `url(${event.thumbnail_url})`}} />
+                <strong>{event.title}</strong>
+                <span>Date: {moment(event.date).format('LL')}</span>
+                <span>Price: {event.price}</span>
+                <span>Description: {event.description}</span>
+                <Button className="submit-btn"> Subscribe </Button>
+                </li>
+            )}
+        </ul>
+    )
+}
